99f0e753f4c324bbc0265b90ac5a0eecc4bde8ca
Marcos Alejandro Moreira Merino <marcos.moreira.ec.dev@gmail.com>
2026-01-02 20:50:56 -0500

    Init: new repository

---
+++ src/main/java/com/marcosmdev/ec/APIRestMain/ApiRestMainApplication.java
@@ -0,0 +1,13 @@
+package com.marcosmdev.ec.APIRestMain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestMainApplication.class, args);
+	}
+
+}
